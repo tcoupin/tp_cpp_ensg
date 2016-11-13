1a0f88e57fe8d42e1db2ef368b6c9a83b1ac3e0c
gpain93 <pain.gregory@gmail.com>
2016-11-13 15:55:00 +0100

    le dernier du TP1

---
+++ src/tp1/pointeurs.cpp
@@ -0,0 +1,22 @@
+#include "pointeurs.h"
+
+void swap(int *pa, int *pb)
+{
+    int tmp;
+    tmp = *pa;
+    *pa = *pb;
+    *pb = tmp;
+}
+
+void minmax(int *p, int n, int *pmin, int *pmax)
+{
+    int *i;
+    *pmin=*p;
+    *pmax=*p;
+
+    for (i=p ; i<p+n ; i++)
+    {
+        if (*i < *pmin) *pmin = *i;
+        if (*i > *pmax) *pmax = *i;
+    }
+}
---
+++ src/tp1/pointeurs.h
@@ -0,0 +1,3 @@
+int swap(int *pa, int *pb);
+
+void minmax(int *p, int n, int *pmin, int *pmax)
